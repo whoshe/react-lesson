802a0231a488f0c0d910498a644e17cc954d78db
Changwoo Nam <whoshe@funnyplan.com>
2022-09-22 17:34:33 +0900

    react code added

---
+++ react-for-beginners2/src/to_do_list.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currnetArray) => [toDo, ...currnetArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  return (
+    <div>
+      <h1>My To Do List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Input your to do..."
+        />
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
